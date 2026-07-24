a46cd58a56fb973b94416653e8e44f810d191a47
luisbastos1965-ux <luisbastos1965@gmail.com>
2026-07-24 09:28:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-// Lógica base da aplicação
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("App Turma inicializada com sucesso!");
-    // Aqui no futuro vamos colocar o código de ligação ao Firebase
-});
+// Importar as ferramentas principais do Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+
+// A tua configuração específica do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAUU6rITouybEamgkPke4UXJwyjMA0nJzU",
+  authDomain: "turmapro-e6358.firebaseapp.com",
+  projectId: "turmapro-e6358",
+  storageBucket: "turmapro-e6358.firebasestorage.app",
+  messagingSenderId: "242512169110",
+  appId: "1:242512169110:web:f94978c0c2a13858a41ab7"
+};
+
+// Inicializar o motor do Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+console.log("Cérebro Firebase ligado com sucesso!");
