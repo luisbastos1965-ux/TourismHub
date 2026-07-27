e537eca450e05cdf89667edd5bd8a3ed311e6e30
luisbastos1965-ux <luisbastos1965@gmail.com>
2026-07-27 01:26:00 +0100

    Initialize Firebase app and export auth and db

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUU6riTOuybEamgkPke4UXJwyjMA0nJzU",
+  authDomain: "turmapro-e6358.firebaseapp.com",
+  projectId: "turmapro-e6358",
+  storageBucket: "turmapro-e6358.firebasestorage.app",
+  messagingSenderId: "242512169110",
+  appId: "1:242512169110:web:f94978c0c2a13858a41ab7"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
