75e6dc990a038f260bbd99b6c6744de050c7429e
luisbastos1965-ux <luisbastos1965@gmail.com>
2026-08-16 23:06:42 +0100

    Update prhf.js

--- js/prof/roles/prhf.js
+++ js/prof/roles/prhf.js
@@ -1,4 +1,4 @@
-import { db } from "./firebase.js";
+import { db } from "../../firebase.js";
 import { doc, getDoc, collection, addDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 import { state, getDisciplinasPermitidas } from "../store.js";
 import { carregarTarefasProf, analisarEAtualizarTurma } from "../ui.js";
