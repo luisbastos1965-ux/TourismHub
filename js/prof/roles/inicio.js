60a590a5065df243347d1720f46ee108accb05de
luisbastos1965-ux <luisbastos1965@gmail.com>
2026-08-16 22:41:44 +0100

    Update inicio.js

--- js/prof/roles/inicio.js
+++ js/prof/roles/inicio.js
@@ -1,13 +1,8 @@
 export async function gerirCliquesInicio(e) {
-    try {
-        if (e.target.closest('#btn-fab-global')) {
-            const fab = document.getElementById('modal-fab-menu');
-            if (fab) fab.style.display = 'flex';
-            return true;
-        }
-        return false;
-    } catch (error) {
-        console.error("Erro no módulo Início:", error);
-        return false;
+    if (e.target.closest('#btn-fab-global')) { 
+        const fab = document.getElementById('modal-fab-menu');
+        if(fab) fab.style.display = 'flex'; 
+        return true; 
     }
+    return false;
 }
